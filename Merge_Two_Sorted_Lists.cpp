f3f3736dcee6bfe31d6730110fe47071e9355863
Chance Luo <247349913+Kunchen-Luo@users.noreply.github.com>
2026-02-07 16:44:40 +0800

    Merge Two Sorted Lists

---
+++ Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0);  //dummy是一个“虚拟”头节点，方便操作。记得初始化
+        ListNode *tail = &dummy;    //tail指针指向已合并链表的最后一个节点
+        while(list1 && list2) {
+            if(list1->val < list2->val) {
+                tail->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                tail->next = list2;
+                list2 = list2->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = list1 ? list1 : list2;
+        return dummy.next;
+    }
+};
+int main(){
+    ListNode* list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+    ListNode* list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+    Solution sol;
+    ListNode* mergedList = sol.mergeTwoLists(list1, list2);
+    while (mergedList != nullptr) {
+        std::cout << mergedList->val << " ";
+        mergedList = mergedList->next;
+    }
+    return 0;
+}
