ed11f3a9f96db3e7704e696f5f04eedb7a89b994
Chance Luo <247349913+Kunchen-Luo@users.noreply.github.com>
2026-02-12 17:09:15 +0800

    Practice: 206.Reverse Linked List

---
+++ 206.Reverse_Linked_List.cpp
@@ -0,0 +1,24 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {};
+    ListNode(int x) : val(x), next(nullptr) {};
+    ListNode(int x, ListNode *next) : val(x), next(nullptr) {};
+};
+
+class Solution_Of_iteration{
+public:
+    ListNode* reverseList(ListNode *head) {
+        if(!head)
+            return nullptr;
+        ListNode *dummy = new ListNode();
+        dummy->next = head;
+        while(head->next) {
+            ListNode *insertnode = head->next;
+            head->next = insertnode->next;
+            insertnode->next = dummy->next;
+            dummy->next = insertnode;
+        }
+        return dummy->next;
+    }
+};
